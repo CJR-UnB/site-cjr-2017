ccdc7a9fc47fb116d9beee59c273eafcd10d689d
VitorRibeiroCustodio <vitorcustodio@cjr.org.br>
2017-11-21 10:50:48 -0200

    Colocando Admin no seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = Admin.new(
+  email: 'vitorcustodio@cjr.org.br',
+  password: '123456',
+  password_confirmation: '123456')
+
+admin.save
